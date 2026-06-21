8c55a5ef1899d0cf6d3b9ed772d5832a59d71b62
oppytut <11549752+oppytut@users.noreply.github.com>
2026-06-21 13:17:01 +0000

    fix: strip CORS origins whitespace

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins.split(","),
+    allow_origins=[o.strip() for o in settings.cors_origins.split(",")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
